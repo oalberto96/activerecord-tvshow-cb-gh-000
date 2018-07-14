22c2fb132c75a9d1596152fe67bba0d9cc80c576
Alberto Ochoa <ochoapublic@gmail.com>
2018-07-14 20:52:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,20 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.where("rating = ?", self.highest_rating).first
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show 
+    self.where("rating = ?", self.lowest_rating)
+  end
+
+  def self.ratings_sum 
+    self.sum(:rating)
+  end
+
+  def self.popular_shows 
+    self.where("rating > 5")
+  end
 end
